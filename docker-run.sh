36a3beb330d780fafbf7cc487ce97db6928bbad9
Amir <AmirIqbal1@users.noreply.github.com>
2024-11-29 10:59:49 +0000

    Update docker-run.sh
    v8

--- docker-run.sh
+++ docker-run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo ""
-
-# First list all avaliable images 
+# Display Docker images
+echo "Available Docker images:"
 docker images
-
 echo ""
 
 # Check if a container name or ID has been provided
 if [ -z "$1" ]; then
-  echo "Please provide a container name or ID"
-  exit 1
+  echo "Please provide a container name or ID to start an interactive shell:"
+  read container_name
+else
+  container_name="$1"
 fi
 
 # Enter the container and start a shell session
-docker exec -it $1 /bin/bash
+docker exec -it "$container_name" /bin/bash
