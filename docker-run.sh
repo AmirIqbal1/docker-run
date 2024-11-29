17f8e16fd48c536c37443ac51b6fca713869482f
Amir <AmirIqbal1@users.noreply.github.com>
2024-11-29 11:05:28 +0000

    Update docker-run.sh
    v10

--- docker-run.sh
+++ docker-run.sh
@@ -13,5 +13,11 @@ else
   container_name="$1"
 fi
 
+# Check if the container exists
+if ! docker inspect -f '{{.State.Status}}' "$container_name" >/dev/null 2>&1; then
+  echo "Error: Container '$container_name' not found."
+  exit 1
+fi
+
 # Enter the container and start a shell session
 docker exec -it "$container_name" /bin/bash
